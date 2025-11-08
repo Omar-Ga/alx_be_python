26bf40c19e94992e7c6d365901c6b7f2367edf07
Omar-Ga <oomarolayan.gamal@gmail.com>
2025-11-08 19:25:28 +0200

    feat: add basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+# Calculate the sum, difference, and product
+addition_result = number1 + number2
+subtraction_result = number1 - number2
+multiplication_result = number1 * number2
+
+# Print the results in the specified format
+print("Addition of", number1, "and", number2, "is", addition_result)
+print("Subtraction of", number1, "and", number2, "is", subtraction_result)
+print("Multiplication of", number1, "and", number2, "is", multiplication_result)
